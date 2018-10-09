57df513b84262068c76c1d87c722687b7e0fa0ae
BeginningToEn <vuongoliver@gmail.com>
2018-10-08 22:03:51 -0700

    Bus Bug fix

--- UniverseP/Units/Bus.java
+++ UniverseP/Units/Bus.java
@@ -1,23 +1,33 @@
 package UniverseP.Units;
 
-import java.util.Optional;
 import java.util.Set;
+import java.util.HashSet;
 
 public class Bus {
     private Integer ID;
     private Location currentLocation;
     private Itinerary myItinerary;
-    private Set<Passenger> currentPassengers;
-    private Set<Passenger> passengersToPickUp;
+    private Set<Integer> currentPassengers;
+    private Set<Integer> passengersToPickUp;
 
     public Bus(int ID, Location currentLocation){
         this.ID = ID;
         this.currentLocation = currentLocation;
         this.myItinerary = Itinerary.createEmptyItinerary();
+        this.currentPassengers = new HashSet<>();
     }
 
     public Location getLocation() {
         return currentLocation;
+    }
+    public int getID(){ return this.ID; }
+
+    public boolean isEmpty(){
+        return currentPassengers.isEmpty();
+    }
+
+    public boolean isUnassigned(){
+        return myItinerary.isEmpty();
     }
 
     public int howManyPassengers() {
@@ -31,13 +41,13 @@ public class Bus {
         }
 
         if (currentLocation.getX() < myItinerary.peek().getX()) {
-            currentLocation.moveDown();
-        } else if (currentLocation.getX() > myItinerary.peek().getX()) {
-            currentLocation.moveUp();
-        } else if (currentLocation.getY() > myItinerary.peek().getY()) {
-            currentLocation.moveLeft();
-        } else if (currentLocation.getY() < myItinerary.peek().getY()) {
             currentLocation.moveRight();
+        } else if (currentLocation.getX() > myItinerary.peek().getX()) {
+            currentLocation.moveLeft();
+        } else if (currentLocation.getY() > myItinerary.peek().getY()) {
+            currentLocation.moveUp();
+        } else if (currentLocation.getY() < myItinerary.peek().getY()) {
+            currentLocation.moveDown();
         }
     }
 
@@ -49,32 +59,38 @@ public class Bus {
         this.myItinerary = myItinerary;
     }
 
-    public boolean isAtActionableLocation() {
+    public void handlePassengers(){
 
-        if ( currentLocation.equals(myItinerary.peek()) ) {
-            return true;
+        while( !myItinerary.isEmpty() && this.reachedDestination() ){
+
+            int passengerID = myItinerary.peek().getPassengerID();
+
+            if (myItinerary.peek().isPickUpLocation()){
+                this.onboard(passengerID);
+            } else {    //drop off location
+                this.deboard(passengerID);
+            }
+
+            myItinerary.poll();
         }
-        return false;
     }
 
-    public void replyToPing() {
-        if ( this.isAtActionableLocation() ) {
-            //positiveResponse
-        } else {
-            //return empty response
-        }
+    public boolean reachedDestination(){
+        ActionableLocation destination = myItinerary.peek();
+        return currentLocation.getX() == destination.getX() && currentLocation.getY() == destination.getY();
     }
 
-    private void negativePingResp() {
-
+    public void onboard(int passengerID){
+        currentPassengers.add(passengerID);
     }
 
-    private void positivePingResp() {
-
+    public void deboard(int passengerID){
+        currentPassengers.remove(passengerID);
     }
 
     @Override
     public String toString(){
-        return "BusID: " + ID + ", Bus Location: " + currentLocation + myItinerary;
+        return "BusID: " + ID + " -- Bus(X,Y): " + currentLocation + " -- Passengers: " + currentPassengers +
+                " -- Itinerary: " + myItinerary;
     }
 }
